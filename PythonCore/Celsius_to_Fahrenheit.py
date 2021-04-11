b6c593a43dd9b6ab3bedb2b086cccb25f9b3df31
sm_moshi <meya@posteo.se>
2021-04-11 19:50:11 +0200

    Update Celsius_to_Fahrenheit.py

--- PythonCore/Celsius_to_Fahrenheit.py
+++ PythonCore/Celsius_to_Fahrenheit.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python
+
+# You are making a Celsius to Fahrenheit converter. 
+# Write a function to take the Celsius value as an argument and return the corresponding Fahrenheit value.
+# Sample Input
+# 36
+# Sample Output 
+# 96.8
+
+print("Enter the temperature in Celsius:")
+celsius = float(input())
+
+def conv(c):
+    return 9/5 * c + 32
+
+fahrenheit = conv(celsius)
+
+print("Your temperature in Fahrenheit:")
+print(fahrenheit)
