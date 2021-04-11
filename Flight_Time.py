98c3e0c4b173f308ec34d31b69182ccb73e569d4
sm_moshi <meya@posteo.se>
2021-04-11 13:43:35 +0200

    Create Flight_Time.py

---
+++ Flight_Time.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# You need to calculate the flight time of an upcoming trip.
+# You are flying from LA to Sydney, covering a distance of 7425 miles, the plane flies at an average speed of 550 miles an hour.
+# Calculate and output the total flight time in hours.
+
+distance = 7425 # Miles
+speed = 550 # Miles per hour
+flight_time = distance / speed
+
+print(flight_time)
