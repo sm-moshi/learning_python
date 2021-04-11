41312f4c7fb48b650fc2e6bc25855642423565ee
sm_moshi <meya@posteo.se>
2021-04-11 15:17:18 +0200

    Create Tip_Calculator.py

---
+++ Tip_Calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# When you go out to eat, you always tip 20% of the bill amount.
+# But who’s got the time to calculate the right tip amount every time?
+# Not you that’s for sure! You’re making a program to calculate tips and save some time.
+# Your program needs to take the bill amount as input and output the tip as a float.
+# Sample Input
+# 50
+# Sample Output
+# 10.0
+
+
+bill = int(input())
+tip = (bill * 20) / 100
+print(tip)
