db8ee91419eb37cfa381b4b2d9fd5d239ca249d9
sm_moshi <meya@posteo.se>
2021-04-11 13:55:19 +0200

    Create Leaderboard.py

---
+++ Leaderboard.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# You need to make a program for a leaderboard. 
+# The program needs to output the numbers 1 to 9, each on a separate line, followed by a dot:
+# 1.
+# 2.
+# 3.
+# ...
+
+print("""
+		  1.
+		  2.
+		  3.
+		  4.
+		  5.
+		  6.
+		  7.
+		  8.
+      9.
+		  """)
