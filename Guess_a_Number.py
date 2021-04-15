7d0e115573c378c7abf3bf29116cdb25f1cf43c7
sm_moshi <meya@posteo.se>
2021-04-15 15:46:18 +0200

    Fixed number guessing game

--- Guess_a_Number.py
+++ Guess_a_Number.py
@@ -7,13 +7,15 @@ print("Please choose a number between 1 and 1000000:")
 for x in range(1):
    value = random.randint(1,1000000)
 
-number = float(input())
+number = int(input())
 
-if number == value:
+if number > 1000000:
+   print("Please choose a number below 1000000!")
+elif number == value:
    print("You chose:", number )
    print("Your CPU chose:", value )
    print("You won!")
-  else:
-    print("You chose:", number )
-    print("Your CPU chose:", value )
-    print("You lost!")
+else:
+   print("You chose:", number )
+   print("Your CPU chose:", value )
+   print("You lost!")
